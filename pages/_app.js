af4edfbeb93a0c81cfdf2091cc22e58f89e0279c
caithermanson <chermanson03@gmail.com>
2025-11-14 12:14:13 -0700

    Refactor main app component and add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
-import CountButton from "../components/CountButton";
-import GitHub from "../components/GitHub";
-import Link from "next/link";
+import CountButton from "../components/CountButton"
+import GitHub from "../components/GitHub"
+import Link from "next/link"
+import Store from "./store"
+import '../styles/global.css'
+import '../styles/CountButton.css'
+import '../styles/SearchBar.css'
+import { useRouter } from 'next/router'
 
-const App = () => {
+const Page = () => {
+    
+    const router = useRouter()
+
+    if (router.pathname === '/') {
     return (
-        <div>
+    <div>
         <Head>
             <title>My NextJS App</title>
         </Head>
@@ -14,14 +23,18 @@ const App = () => {
             Welcome to Caitlin's NextJS App!
         </h1>
 
-        <CountButton />
-        <CountButton />
+        <CountButton incrementBy={1} buttonColor={'lightblue'}/>
+        <CountButton incrementBy={2} buttonColor={'lightblue'}/>
 
         <GitHub />
-
+        
         <p><Link href="/store">Fake Store</Link> Link</p>
     </div>
     )
+    } else {
+        return (
+            <Store />
+        )
+    }
 }
-
-export default App
+export default Page 
