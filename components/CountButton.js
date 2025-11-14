0dc9ba57412fe0b50c39643f519ff74f8c51a358
caithermanson <chermanson03@gmail.com>
2025-11-14 12:16:04 -0700

    Refactor CountButton to use props for increment and style
    Updated CountButton to accept increment value and button color as props. Added useEffect to reset count after exceeding 10.

--- components/CountButton.js
+++ components/CountButton.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const CountButton = () => {
+const CountButton = (props) => {
 
     const [currentCount, setCurrentCount] = useState(0)
 
     const handleClick = () => {
-        setCurrentCount(currentCount + 1)
+        setCurrentCount(currentCount + props.incrementBy)
+    }
+
+    useEffect(() => {
+        if(currentCount > 10) {
+            setCurrentCount(0)
+        }
+    }, [currentCount])
+
+    const buttonStyle = {
+        background: props.buttonColor,
+        borderRadius: '10%',
+        padding: '10px',
+        margin: '5px',
     }
 
     return (
         <div>
-            <button onClick={handleClick}>+1</button>
-            <div>{currentCount}</div>
+            <button style={buttonStyle} onClick={handleClick}>+{props.incrementBy}</button>
+            <div className={'count-display'}>{currentCount}</div>
         </div>
     )
 }
